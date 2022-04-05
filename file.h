be3578bf98b83bcc917bb3c7934d067074293d1c
Alessandro <alessandro.bardossi@stud.unifi.it>
2022-04-05 20:45:00 +0200

    Aggiunto salvataggio file

--- file.h
+++ file.h
@@ -4,28 +4,6 @@
 
 #ifndef FILE_INI_FILE_H
 #define FILE_INI_FILE_H
-#include <vector>
-#include <fstream>
-#include <string>
-#include <iostream>
-
-using namespace std;
-
-
-class ReadFileIni{
-
-public:
-    bool OpenFileIni(string nameFile);
-    void ReadOpenFile();
-    int VideoVisualize();
-    void SaveOpenFile(string name_of_file_output);
-
-private:
-    fstream File_in_input;
-    filebuf File_in_output;
-    vector<string> line_Of_File;
-
-};
 
 
 #endif //FILE_INI_FILE_H
