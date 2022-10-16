6652b50d1b9a2cd0c451f5bb21e91012ea3ab302
Alessandro <alessandro.bardossi@stud.unifi.it>
2022-10-16 15:20:01 +0200

    set part of the test

--- test/ReadFileiniTest.cpp
+++ test/ReadFileiniTest.cpp
@@ -1,4 +1,12 @@
 //
 // Created by Alessandro on 19/04/2022.
 //
+#include "gtest/gtest.h"
 
+#include "../ReadfileIni.h"
+
+TEST(ReadFileIni,defaultreadSection){
+    string name_file_ini="test.ini";
+    ReadFileIni file_ini;
+    ASSERT_EQ("[section3]",file_ini.readSection("[section3]"));
+}
